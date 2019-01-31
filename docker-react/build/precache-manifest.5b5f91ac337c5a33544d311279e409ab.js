50686b5e19d2c9bd45a236e44d0d1c465301ec98
Venkatesh G <itsvenkatesh@gmail.com>
2019-01-31 17:53:09 +0000

    travis-ci integration

---
+++ docker-react/build/precache-manifest.5b5f91ac337c5a33544d311279e409ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "057786a0142edd95604b",
+    "url": "/static/js/main.057786a0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "057786a0142edd95604b",
+    "url": "/static/css/main.8d8b0297.chunk.css"
+  },
+  {
+    "revision": "b55de7956ca13f1b4425310346da332c",
+    "url": "/index.html"
+  }
+];
